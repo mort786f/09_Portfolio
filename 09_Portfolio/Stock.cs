c1e27f8d662d60928bca94a072e5f0d823424dea
morte <morte@DESKTOP-AJQ2HRF>
2016-09-26 10:59:54 +0200

    Add project files.

---
+++ 09_Portfolio/Stock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _09_Portfolio
+{
+    internal class Stock
+    {
+        public Stock()
+        {
+            
+        }
+
+        public int NumShares { get; internal set; }
+        public double PricePerShare { get; internal set; }
+        public string Symbol { get; internal set; }
+
+        internal int GetValue()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
